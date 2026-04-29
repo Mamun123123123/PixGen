e854e83c9aabdb4acb638b0f05fbef519f14a17c
Mamun123123123 <almalm90459@gmail.com>
2026-04-29 23:01:27 +0600

    profile page done and update method use

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,84 +1,75 @@
 "use client";
 import { authClient } from "@/lib/auth-client";
+import { Avatar, Button } from "@heroui/react";
 import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
-  const { data: session, isPending } = authClient.useSession();
-  const user = session?.user;
- 
- 
+  const userData = authClient.useSession();
+  const user = userData.data?.user;
+
+  const handleSignOut = async () => {
+    await authClient.signOut();
+  }
+
   return (
     <div className="border-b px-2">
-      <nav className="flex justify-between items-center py-3 max-w-7xl mx-auto w-full">
-
-
+      <nav className=" flex justify-between items-center  py-3 max-w-7xl mx-auto w-full">
         <div className="flex gap-2 items-center">
           <Image
-            src="/logo.png"
+            src={"/logo.png"}
             alt="logo"
+            loading="eager"
             width={30}
             height={30}
-            className="object-cover"
+            className="object-cover h-auto w-auto"
           />
           <h3 className="font-black text-lg">pixgen.</h3>
         </div>
 
-
         <ul className="flex items-center gap-5 text-sm">
           <li>
-            <Link href="/">Home</Link>
+            <Link href={"/"}>Home</Link>
           </li>
           <li>
-            <Link href="/all-photos">All Photos</Link>
+            <Link href={"/all-photos"}>All Photos</Link>
           </li>
           <li>
-            <Link href="/pricing">Pricing</Link>
+            <Link href={"/pricing"}>Pricing</Link>
           </li>
-
-         
-          {user && (
-            <li>
-              <Link href="/profile">Profile</Link>
-            </li>
-          )}
+          {user && <li>
+            <Link href={"/profile"}>Profile</Link>
+          </li> }
+          
         </ul>
 
-      
-        <div className="flex items-center gap-4 text-sm">
-          {isPending ? (
-            <span className="loading loading-spinner loading-lg"></span>
-          ) : user ? (
-            <div className="flex items-center gap-3">
-              
-            
-              <Image
-                src={user?.image || "/default.png"}
-                alt="user"
-                width={32}
-                height={32}
-                className="rounded-full"
-              />
+        <div className="flex gap-4">
+          {!user && (
+            <ul className="flex items-center  text-sm gap-5">
+              <li>
+                <Link href={"/signup"}>SignUp</Link>
+              </li>
+              <li>
+                <Link href={"/signin"}>SignIn</Link>
+              </li>
+            </ul>
+          )}
 
-           
-              <div className="flex flex-col leading-tight">
-                <span className="font-medium">{user?.name}</span>
-                <button
-                  onClick={() => authClient.signOut()}
-                  className="text-xs text-red-500 text-left"
-                >
-                  Logout
-                </button>
-              </div>
+          {user && (
+            <div className="flex gap-3">
+              <Avatar size="sm">
+                <Avatar.Image
+                  alt="John Doe"
+                  src={user?.image}
+                  referrerPolicy="no-referrer"
+                />
+                <Avatar.Fallback>{user?.name.charAt(0)}</Avatar.Fallback>
+              </Avatar>
+
+              <Button onClick={handleSignOut} size="sm" variant="danger">SignOut</Button>
             </div>
-          ) : (
-            <>
-              <Link href="/signup">SignUp</Link>
-              <Link href="/signin">SignIn</Link>
-            </>
           )}
         </div>
-
       </nav>
     </div>
   );
